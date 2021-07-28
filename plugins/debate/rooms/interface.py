3e69b58879faa0f3a78a5c5322104d77aa9677cf
taven <8206808+daegontaven@users.noreply.github.com>
2021-07-28 14:34:45 +0530

    Stop building embed at 10 members

--- plugins/debate/rooms/interface.py
+++ plugins/debate/rooms/interface.py
@@ -1268,7 +1268,7 @@ class DebateRooms(commands.Cog, name="Debate"):
         description = ""
         count = 0
         async for elo_mapping in elo_cursor:
-            if count == 11:
+            if count == 10:
                 break
             member = guild.get_member(elo_mapping["member_id"])
             if not member:
